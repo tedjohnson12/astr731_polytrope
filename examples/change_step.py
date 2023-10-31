d3af1ca2c7944a1710905214908059886810f838
Ted Johnson <ted.johnson@unlv.edu>
2023-10-30 22:26:57 -0700

    update step example

--- examples/change_step.py
+++ examples/change_step.py
@@ -2,14 +2,14 @@ from pathlib import Path
 import numpy as np
 import matplotlib.pyplot as plt
 
-from polysolver import solve
+from polysolver import solve_rust as solve
 
 
 if __name__ in '__main__':
-    n = 0
+    n = 4
     x_init = 0.01
-    for step_size in np.logspace(-3, 0, 11):
-        x, y = solve(x_init=x_init, n=n, h=step_size, max_iter=1000)
+    for step_size in np.linspace(0.01, 0.4, 11):
+        x, y, _ = solve(x_init=x_init, n=n, h=step_size, max_iter=1000)
 
         plt.plot(x, y, label=f'log($\\Delta \\xi$)={np.log10(step_size):.2f}')
 
