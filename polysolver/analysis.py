40afb51abe69a4f5b0007d4915e574f68732ec24
Ted Johnson <ted.johnson@unlv.edu>
2023-12-05 21:34:29 -0800

    add star class and some classmethods

--- polysolver/analysis.py
+++ polysolver/analysis.py
@@ -24,7 +24,10 @@ where
 
 
 """
+import warnings
 import numpy as np
+
+from polysolver import solve
 
 def get_rho_norm(y:np.ndarray,n:float)->np.ndarray:
     """
@@ -138,3 +141,162 @@ def central_over_mean_density(x,y,n):
     mass_over_rhoc = norm_mass(x,y,n)
     vol = volume(x)
     return vol/mass_over_rhoc
+
+class Star:
+    """
+    A polytropic star
+    
+    Parameters
+    ----------
+    x : np.ndarray
+        The x values. Recall that :math:`x=\\xi=\\frac{r}{r_n}`
+    y : np.ndarray
+        The y values. Recall that :math:`y=\\theta_n`
+        and :math:`\\rho(r) = \\rho_c \\theta^n(r)`
+    z : np.ndarray
+        The z values. Recall that :math:`z=\\frac{d\\theta_n}{d\\xi}`
+    n : float
+        The index of the polytrope.
+    """
+    def __init__(
+        self,
+        x:np.ndarray,
+        y:np.ndarray,
+        z:np.ndarray,
+        n:float
+    ):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.n = n
+    @classmethod
+    def from_soln(
+        cls,
+        x_init:float,
+        n:float,
+        h:float,
+        max_iter:int=1000,
+        impl:str='rust'
+    ):
+        """
+        Create a star from a solution to the Lane-Emden equation.
+        
+        Parameters
+        ----------
+        x_init : float
+            The initial x value. Choose something small.
+        n : float
+            The index of the polytrope.
+        h : float
+            The step size. This should be less than the pressure scale height.
+        max_iter : int, optional
+            The maximum number of iterations. The default is 1000.
+        impl : str, optional
+            The implementation to use. The default is 'rust'.
+        
+        Returns
+        -------
+        Star
+            The star.
+        """
+        x,y,z = solve(x_init,n,h,max_iter,impl)
+        return cls(x,y,z,n)
+    @classmethod
+    def _zero(cls,x:np.ndarray):
+        """
+        Analytic solution to the Lane-Emden equation
+        for :math:`n=0`.
+        
+        Parameters
+        ----------
+        x : np.ndarray
+            The x values. Recall that :math:`x=\\xi=\\frac{r}{r_n}`
+        
+        Notes
+        -----
+        This is the constant density model.
+        
+        .. math::
+            y = 1 - \\frac{x^2}{6}
+        
+        .. math::
+            z = \\frac{-x}{3}
+        
+        .. math::
+            \\xi_1 = \\sqrt{6}
+        """
+        xmin = 0
+        xmax = np.sqrt(6)
+        if np.any(x<xmin):
+            warnings.warn(f'Analytic solution only valid for x>{xmin:.2f}',RuntimeWarning)
+        if np.any(x>xmax):
+            warnings.warn(f'Analytic solution only valid for x<={xmax:.2f}',RuntimeWarning)
+        x = np.where((x<xmin)|(x>xmax),x,np.nan)
+        y = 1 - x**2/6
+        z = -x/3
+        return cls(x,y,z,0)
+    @classmethod
+    def _one(cls,x:np.ndarray):
+        """
+        Analytic solution to the Lane-Emden equation
+        for :math:`n=1`.
+        
+        Parameters
+        ----------
+        x : np.ndarray
+            The x values. Recall that :math:`x=\\xi=\\frac{r}{r_n}`
+        
+        Notes
+        -----
+        This is the sinc function.
+        Note that the numpy implementation
+        of sinc implicitly multiplies by :math:`\\pi`.
+        
+        .. math::
+            y = \\frac{\\sin(x)}{x}
+        
+        .. math::
+            z = \\frac{\\cos(x)-\\sin(x)/x}{x}
+        
+        except :math:`z=0` for :math:`x=0`.
+            
+        .. math::
+            \\xi_1 = \\pi
+        """
+        xmin = 0
+        xmax = np.pi
+        if np.any(x<xmin):
+            warnings.warn(f'Analytic solution only valid for x>{xmin:.2f}',RuntimeWarning)
+        if np.any(x>xmax):
+            warnings.warn(f'Analytic solution only valid for x<={xmax:.2f}',RuntimeWarning)
+        x = np.where((x<xmin)|(x>xmax),x,np.nan)
+        y = np.sinc(x/np.pi)
+        z = np.where(x==0,0,(np.cos(x)-np.sinc(x/np.pi))/x)
+        return cls(x,y,z,1)
+    @classmethod
+    def analytic(
+        cls,
+        x:np.ndarray,
+        n:int
+    ):
+        """
+        Get the analytic solution to the Lane-Emden equation.
+        
+        Parameters
+        ----------
+        x : np.ndarray
+            The x values. Recall that :math:`x=\\xi=\\frac{r}{r_n}`
+        n : float
+            The index of the polytrope.
+        
+        Returns
+        -------
+        Star
+            The star.
+        """
+        if n==0:
+            return cls._zero(x)
+        elif n==1:
+            return cls._one(x)
+        else:
+            raise NotImplementedError(f'There is no analytic solution for n={n:d}')
